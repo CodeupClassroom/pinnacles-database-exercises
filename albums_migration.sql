181a513bc31e5eeadf6beeeb005aa2737c4a9ce4
Zach Gulde <zachgulde@gmail.com>
2017-05-31 11:39:03 -0500

    create albums migration

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,14 @@
 -- create the albums table
+
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  artist VARCHAR(255),
+  name VARCHAR(255),
+  release_date INTEGER,
+  sales FLOAT,
+  genre VARCHAR(255)
+);
