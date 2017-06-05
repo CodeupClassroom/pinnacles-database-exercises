76913b8184b0f858d0432583496586aa3c7f594a
Zach Gulde <zachgulde@gmail.com>
2017-06-05 12:19:09 -0500

    finish subqueries exercise

--- subqueries.sql
+++ subqueries.sql
@@ -39,3 +39,27 @@ where gender = 'F'
 
 -- use a subquery when you only need information from one table
 -- but need filter results by another table
+
+-- Find all the employees with the same hire date as employee 101010 using a sub-query.
+
+select *
+from employees where hire_date = (
+  select hire_date from employees where emp_no = 101010
+);
+
+-- Find all the titles held by all employees with the first name Aamod.
+
+select title
+from titles
+where emp_no in (
+  select emp_no from employees
+  where first_name = 'Aamod'
+);
+
+-- Find all the department managers that are female.
+
+select * from employees
+where gender = 'F'
+  and emp_no in (
+  select emp_no from dept_manager where to_date > now()
+);
